1175de1fc4615046aa042f2d50633de04c3908ee
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2021-05-05 16:28:41 +0300

    Initial

---
+++ stream-listener/src/main/java/com/bala/kafkastreams/streamlistener/StreamListenerApplication.java
@@ -0,0 +1,13 @@
+package com.bala.kafkastreams.streamlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamListenerApplication.class, args);
+	}
+
+}
---
+++ stream-listener/src/test/java/com/bala/kafkastreams/streamlistener/StreamListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.kafkastreams.streamlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
