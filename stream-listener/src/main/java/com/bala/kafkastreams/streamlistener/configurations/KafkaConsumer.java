f777e0a57d2dc656f6bbc5fc0013d100c6c298da
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2021-05-06 10:38:32 +0300

    Log4j2 added

--- stream-listener/src/main/java/com/bala/kafkastreams/streamlistener/configurations/KafkaConsumer.java
+++ stream-listener/src/main/java/com/bala/kafkastreams/streamlistener/configurations/KafkaConsumer.java
@@ -1,25 +1,26 @@
 package com.bala.kafkastreams.streamlistener.configurations;
 
 import org.apache.kafka.streams.kstream.KStream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.log4j.Log4j2;
+
+import java.util.function.Consumer;
+
+@Log4j2
 @Configuration
-public class Consumer {
+public class KafkaConsumer {
 	
 	//https://docs.spring.io/spring-cloud-stream-binder-kafka/docs/3.1.0/reference/html/spring-cloud-stream-binder-kafka.html#_functional_style
 	//https://stackoverflow.com/questions/66881210/spring-cloud-stream-kafka-streams-binder-kafkaexception-could-not-start-stream
-		
-	Logger logger=LoggerFactory.getLogger(Consumer.class);
-	
 	
 	@Bean
-    public java.util.function.Consumer<KStream<String, String>> kstreamConsumer() { // This method name is used in application.properties
-		java.util.function.Consumer<KStream<String,String>> consumer= (KStream<String,String> kStream) -> {
+    public Consumer<KStream<String, String>> kstreamConsumer() { // This method name is used in application.properties
+		Consumer<KStream<String,String>> consumer= (KStream<String,String> kStream) -> {
 			kStream.foreach((key, value) -> {
-				logger.info("Key: " + key + " Value: " + value);
+				log.info("Key: " + key + " Value: " + value);
 			});
 		};
 		return consumer;
